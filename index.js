8301a81a9bb162c2a8db03c53719893c6c8f9402
sindiga-dev <isaac.sindiga@student.moringaschool.com>
2022-08-07 22:31:24 +0300

    X

---
+++ index.js
@@ -0,0 +1,35 @@
+//function add 
+function add(a, b){
+    return a+b;
+}
+//subtract
+function subtract(a, b){
+    return a-b;
+}
+//function multiply
+function multiply(a, b){
+    return a*b;
+}
+//function divide
+function divide(a, b){
+    return a/b;
+}
+function increment(n){
+    return n+1;
+}
+
+function decrement(n){
+    return n-1;
+}
+
+function makeInt(n) {
+    let a = parseInt(n, 10);
+    return a;
+  }
+  
+  // floating number return
+  
+  function preserveDecimal(n) {
+    let a = parseFloat(n);
+    return a;
+  }
